9191c07a5ff5def8f78deced852f28d305483ecf
allan81 <rjaycondiman81@gmail.com>
2023-08-27 01:09:04 +0800

    Update with table loadConTest

--- loadConTest.php
+++ loadConTest.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 //error_reporting(E_ERROR | E_WARNING);
 //include 'includes/saveSession.php';
@@ -8,11 +11,11 @@ include 'includes/factset.php';
 $fc = new factset();
 
 $fields = array(
-	'FirstName'	=> 'Mark',
-	'LastName'	=> 'Schneider',
-	'Email'		=> 'schneiders11@hotmail.com',
-	'Password'	=> '44444444',
-	'Price'	=> '44444444'
+    'FirstName'	=> 'Mark',
+    'LastName'	=> 'Schneider',
+    'Email'		=> 'schneiders11@hotmail.com',
+    'Password'	=> '44444444',
+    'Price'	=> '44444444'
 );
 
 
@@ -37,17 +40,50 @@ $grpID = $fc->getFundamentals('AAPL-USA','FF_SALES','2019-07-30','2020-07-30','A
 // echo "<pre>";
 // print_r($grp);
 // echo "</pre>";
-
-
-$responseBody = $rollingConsensusData->getBody()->getContents();
-// $dataArray = json_encode($rollingConsensusData, true);
-
-
-if ($responseBody === null) {
-    echo "Error decoding JSON response.";
-} else {
-    echo($responseBody);
-}
-
-
 ?>
+
+<div class="container mt-5">
+    <h2>Rolling Consensus Data</h2>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Request ID</th>
+            <th>FSYM ID</th>
+            <th>Metric</th>
+            <th>Fiscal Year</th>
+            <th>Fiscal End Date</th>
+            <th>Mean</th>
+            <th>Median</th>
+            <th>Standard Deviation</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $responseBody = $rollingConsensusData->getBody()->getContents();
+
+        if (!empty($responseBody)) {
+            $data = json_decode($responseBody, true);
+            if (isset($data['data'])) {
+                // Iterate through the data and display it in the table
+                foreach ($data['data'] as $row) {
+                    echo '<tr>';
+                    echo '<td>' . $row['requestId'] . '</td>';
+                    echo '<td>' . $row['fsymId'] . '</td>';
+                    echo '<td>' . $row['metric'] . '</td>';
+                    echo '<td>' . $row['fiscalYear'] . '</td>';
+                    echo '<td>' . $row['fiscalEndDate'] . '</td>';
+                    echo '<td>' . $row['mean'] . '</td>';
+                    echo '<td>' . $row['median'] . '</td>';
+                    echo '<td>' . $row['standardDeviation'] . '</td>';
+                    echo '</tr>';
+                }
+            } else {
+                echo '<tr><td colspan="8">No data available</td></tr>';
+            }
+        } else {
+            echo '<tr><td colspan="8">Error retrieving data</td></tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
