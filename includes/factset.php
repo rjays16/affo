a3e3047007d54923d441d3cdc32d18448519da93
allan81 <rjaycondiman81@gmail.com>
2023-08-29 12:03:43 +0800

    Push New Codes For getting grp and dividend

--- includes/factset.php
+++ includes/factset.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'vendor/autoload.php';
 use GuzzleHttp\Client as GuzzleClient;
 
@@ -14,7 +14,7 @@ class factset {
     private $cacheDBUser;
     private $cacheDBPass;
     private $cacheDBMode = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-    
+
     public function __construct() {
         if ($_SERVER['SERVER_ADDR'] == '134.209.40.60') {
             $envPrefix = 'members';
@@ -31,11 +31,11 @@ class factset {
             'timeout' => 6.0,
             'http_errors' => false
         ]);
-        
+
         $this->cacheDB = 'mysql:host='.$secretCFG['db']['host'].';dbname='.$secretCFG['db']['dbname'].';charset=utf8';
         $this->cacheDBUser = $secretCFG['db']['dbuser'];
         $this->cacheDBPass = $secretCFG['db']['dbpass'];
-        
+
         try {
             $dbstring = $this->cacheDB;
             $this->db = new PDO($dbstring, $this->cacheDBUser, $this->cacheDBPass, $this->cacheDBMode);
@@ -45,7 +45,7 @@ class factset {
 
         unset($secretCFG);
     }
-    
+
 //     public function getRollingConsensus($asset_id, $metrics, $start_date, $end_date, $frequency, $relative_fiscal_start, $relative_fiscal_end, $periodicity, $currency) {
 //         $endpoint = "/content/factset-estimates/v2/rolling-consensus";
 //         $params = array();
@@ -104,7 +104,7 @@ class factset {
             ),
             'debug' => false
         );
-    
+
         try {
             $response = $this->gzClient->request('POST', $endpoint, $params);
             return $response;
@@ -113,68 +113,61 @@ class factset {
             return $e->getResponse();
         }
     }
-    
+
 
 
     public function getFundamentals($asset_id, $metrics, $start_date, $end_date, $periodicity, $currency) {
-        $endpoint = "/content/factset-fundamentals/v1/fundamentals";
-        $params = array();
-        $params['headers'] = array(
-            'Accept'     => 'application/json',
-            'Content-Type' => 'application/json'
+        $endpoint = $baseURL."/content/factset-fundamentals/v1/fundamentals";
+        $params = array(
+            $params = array(
+                'Accept'     => 'application/json',
+                'Content-Type' => 'application/json'
+            ),
+            'auth' => array($this->apiSerial, $this->apiKey),
+            'json'=> array(
+                'ids' => array($asset_id),
+                'metrics' => array($metrics),
+                'fiscalPeriodStart' => $start_date,
+                'fiscalPeriodEnd' => $end_date,
+                'periodicity' => $periodicity,
+                'currency' => $currency
+            ),
+            'debug' => false
         );
-        $params['auth'] = array($this->apiSerial, $this->apiKey);
-        $params['debug'] = false;
-        $params['json'] = array(
-            'ids' => [$asset_id],
-            'metrics' => [$metrics],
-            'fiscalPeriodStart' => $start_date,
-            'fiscalPeriodEnd' => $end_date,
-            'periodicity' => $periodicity,
-            'currency' => $currency
-        );
 
-        $response = $this->gzClient->request('GET', $endpoint, $params);
-
-        //var_dump($response);
-        //echo $response->getBody();
-
-        return json_decode($response->getBody());
-    /*
-        if ($http_code == 200) {
-            $data = json_decode($response, true);
-            return $data;
-        } else {
-            return "API request failed: HTTP $http_code";
+        try {
+            $response = $this->gzClient->request('POST', $endpoint, $params);
+            return $response;
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            // Handle the exception if needed
+            return $e->getResponse();
         }
-    */
+
     }
 
-    public function DividendPerShare($asset_id, $category, $subcategory, $start_date, $end_date, $frequency, $relative_fiscal_start, $relative_fiscal_end, $periodicity, $currency) {
-        $endpoint = "/content/factset-estimates/v2/metrics";
-        $params = array();
-        $params['headers'] = array(
-            'Accept'     => 'application/json',
-            'Content-Type' => 'application/json'
+    public function Dividend($asset_id,  $currency) {
+        $endpoint = $baseURL."/content/factset-global-prices/v1/annualized-dividends";
+        $params = array(
+            'headers' => array(
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ),
+            'auth' => array($this->apiSerial, $this->apiKey),
+            'json' => array(
+                'ids' => array($asset_id),
+                'currency' => $currency
+            ),
+            'debug' => false
         );
-        $params['auth'] = array($this->apiSerial, $this->apiKey);
-        $params['debug'] = false;
-        $params['query'] = array(
-            'ids' => [$asset_id],
-            'category' => $category,
-            'subcategory' => $subcategory,
-            'startDate' => $start_date,
-            'endDate' => $end_date,
-            'frequency' => $frequency,
-            'relativeFiscalStart' => $relative_fiscal_start,
-            'relativeFiscalEnd' => $relative_fiscal_end,
-            'periodicity' => $periodicity,
-            'currency' => $currency
-        );
-    
-        $response = $this->gzClient->request('GET', $endpoint, $params);
 
-        return $response;
+        try {
+            $response = $this->gzClient->request('POST', $endpoint, $params);
+            return $response;
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            // Handle the exception if needed
+            return $e->getResponse();
+        }
     }
+
 }
 ?>
--- loadConTest.php
+++ loadConTest.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <?php
 //error_reporting(E_ERROR | E_WARNING);
 //include 'includes/saveSession.php';
@@ -18,94 +19,81 @@ $fields = array(
     'Price'	=> '44444444'
 );
 
-
-$asset_id = 'AAPL-USA';
-$metrics = 'SALES';
-$start_date = '2018-01-01';
-$end_date = '2023-08-25';
-$frequency = 'D';
-$relative_fiscal_start = 1;
-$relative_fiscal_end = 3;
-$periodicity = 'ANN';
-$currency = 'USD';
-
-$rollingConsensusData = $fc->getRollingConsensus($asset_id, $metrics, $start_date, $end_date, $frequency, $relative_fiscal_start, $relative_fiscal_end, $periodicity, $currency);
-
 //($asset_id, $metrics, $start_date, $end_date, $frequency, $relative_fiscal_start, $relative_fiscal_end, $periodicity, $currency)
 //$cust = $fc->getRollingConsensus('AAPL-USA','SALES','2019-07-30','2020-07-30','AY',1,2,'ANN','USD');
 
 //($asset_id, $metrics, $start_date, $end_date, $frequency, $periodicity, $currency)
-$grpID = $fc->getFundamentals('AAPL-USA','FF_SALES','2019-07-30','2020-07-30','ANN','USD');
+$rollingConsensusID = $fc->getRollingConsensus('AAPL-USA', 'SALES', '2018-01-01','2023-08-25', 'D', 1, 3, 'ANN', 'USD');
+$grpID = $fc->getFundamentals('AAPL-USA','FF_SALES','2018-01-30','2023-08-25','ANN','USD');
+$dividendID = $fc->Dividend('AAPL-USA', 'USD');
 
-// echo "<pre>";
-// print_r($grp);
-// echo "</pre>";
+$rollingConsensusBody = $rollingConsensusID->getBody()->getContents(); // for retrieving rolling consensus data
+$grpIDBody = $grpID->getBody()->getContents(); // for retrieving fundamentals
+$dividendBody =  $dividendID->getBody()->getContents(); // for retrieving dividendPerShare
+
+echo $dividendBody;
 ?>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<div class="container mt-5">
+<!-- <div class="container mt-5">
     <h2>Rolling Consensus Data</h2>
     <table class="table table-striped">
         <thead>
-        <tr>
-            <th>Request ID</th>
-            <th>FSYM ID</th>
-            <th>Metric</th>
-            <th>Fiscal Year</th>
-            <th>Fiscal End Date</th>
-            <th>Mean</th>
-            <th>Median</th>
-            <th>Standard Deviation</th>
-        </tr>
+            <tr>
+                <th>Request ID</th>
+                <th>FSYM ID</th>
+                <th>Metric</th>
+                <th>Fiscal Year</th>
+                <th>Fiscal End Date</th>
+                <th>Mean</th>
+                <th>Median</th>
+                <th>Standard Deviation</th>
+            </tr>
         </thead>
         <tbody>
-        <?php
-        $responseBody = $rollingConsensusData->getBody()->getContents();
+            <?php
+$responseBody = $rollingConsensusData->getBody()->getContents();
 
-        if (!empty($responseBody)) {
-            $data = json_decode($responseBody, true);
-            if (isset($data['data'])) {
-                $perPage = 10;
-                $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
-                $totalItems = count($data['data']);
-                $startIndex = ($currentPage - 1) * $perPage;
-                $dataSlice = array_slice($data['data'], $startIndex, $perPage);
+if (!empty($rollingConsensusBody)) {
+    $data = json_decode($rollingConsensusBody, true);
+    if (isset($data['data'])) {
+        $perPage = 150;
+        $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+        $totalItems = count($data['data']);
+        $startIndex = ($currentPage - 1) * $perPage;
+        $dataSlice = array_slice($data['data'], $startIndex, $perPage);
 
-                // Iterate through the data slice and display it in the table
-                foreach ($dataSlice as $row) {
-                    echo '<tr>';
-                    echo '<td>' . $row['requestId'] . '</td>';
-                    echo '<td>' . $row['fsymId'] . '</td>';
-                    echo '<td>' . $row['metric'] . '</td>';
-                    echo '<td>' . $row['fiscalYear'] . '</td>';
-                    echo '<td>' . $row['fiscalEndDate'] . '</td>';
-                    echo '<td>' . $row['mean'] . '</td>';
-                    echo '<td>' . $row['median'] . '</td>';
-                    echo '<td>' . $row['standardDeviation'] . '</td>';
-                    echo '</tr>';
-                }
-            } else {
-                echo '<tr><td colspan="8">No data available</td></tr>';
-            }
-        } else {
-            echo '<tr><td colspan="8">Error retrieving data</td></tr>';
+        // Iterate through the data slice and display it in the table
+        foreach ($dataSlice as $row) {
+            echo '<tr>';
+            echo '<td>' . $row['requestId'] . '</td>';
+            echo '<td>' . $row['fsymId'] . '</td>';
+            echo '<td>' . $row['metric'] . '</td>';
+            echo '<td>' . $row['fiscalYear'] . '</td>';
+            echo '<td>' . $row['fiscalEndDate'] . '</td>';
+            echo '<td>' . $row['mean'] . '</td>';
+            echo '<td>' . $row['median'] . '</td>';
+            echo '<td>' . $row['standardDeviation'] . '</td>';
+            echo '</tr>';
         }
-        ?>
+    } else {
+        echo '<tr><td colspan="8">No data available</td></tr>';
+    }
+} else {
+    echo '<tr><td colspan="8">Error retrieving data</td></tr>';
+}
+?>
         </tbody>
     </table>
-    <nav aria-label="Pagination">
-        <ul class="pagination">
-            <?php
-            // Calculate and generate pagination links
-            $totalPages = ceil($totalItems / $perPage);
-            for ($page = 1; $page <= $totalPages; $page++) {
-                $activeClass = ($page == $currentPage) ? ' active' : '';
-                echo '<li class="page-item' . $activeClass . '"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
-            }
-            ?>
-        </ul>
-    </nav>
-</div>
+	  <nav aria-label="Pagination">
+            <ul class="pagination">
+                <?php
+// Calculate and generate pagination links
+$totalPages = ceil($totalItems / $perPage);
+for ($page = 1; $page <= $totalPages; $page++) {
+    $activeClass = ($page == $currentPage) ? ' active' : '';
+    echo '<li class="page-item' . $activeClass . '"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
+}
+?>
+            </ul>
+        </nav>
+</div> -->
